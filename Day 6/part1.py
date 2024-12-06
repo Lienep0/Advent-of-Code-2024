27aa985174cf88ceb83cd15cac36689240ae35f2
Lienep <neilp@live.fr>
2024-12-06 16:54:03 +0100

    Finished part 1

---
+++ Day 6/part1.py
@@ -0,0 +1,45 @@
+DIRECTIONS = [(-1, 0), (0, 1), (1, 0), (0, -1)] 
+board = [list(line.removesuffix("\n")) for line in open("input.txt").readlines()]
+height = len(board)
+width = len(board[0])
+
+def find_startpos(board):
+    for i, row in enumerate(board):
+        for j, value in enumerate(row):
+            if value == "^":
+                return (i, j)
+            
+def count_crosses(board):
+    s = 0
+    for i, row in enumerate(board):
+        for j, value in enumerate(row):
+            if value == "X":
+                s += 1
+    return s
+
+def path(board, start):
+    i, j = start
+    direction_counter = 0
+    board[i][j] = "X"
+
+    while True:
+        i_dir, j_dir = DIRECTIONS[direction_counter % 4]
+        i += i_dir
+        j += j_dir
+
+        if not (i >= 0 and i < height and j >= 0 and j < width):
+            return board
+
+        elif board[i][j] == "#":
+            direction_counter += 1
+            i -= i_dir
+            j -= j_dir
+
+        else :
+            board[i][j] = "X"
+
+new_board = path(board, find_startpos(board))
+for line in new_board :
+    print(''.join(line))
+
+print(count_crosses(new_board))
