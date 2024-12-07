1b2efe42076b5e33ea17d26ff5d393eeafbc5af2
Lienep <neilp@live.fr>
2024-12-07 18:52:01 +0100

    Finished part 2 (kept the same file since it was so similar to part 1)

--- Day 7/part1.py
+++ Day 7/part1.py
@@ -2,24 +2,25 @@ equations = []
 
 for line in open("input.txt").readlines():
     equation = line.split(":")
-    equation[1] = equation[1].removeprefix(" ").removesuffix("\n").split(" ")
+    equation[1] = equation[1][1:-1].split(" ")
     equations.append((int(equation[0]), [int(x) for x in equation[1]]))
 
 def solve_equation(value, subvalues, current, depth, maxdepth):
-    new_plus = current + subvalues[depth]
-    new_mult = current * subvalues[depth]
+    operand = subvalues[depth]
 
-    if new_plus == value or new_mult == value:
-        return value
+    new_plus = current + operand
+    new_mult = current * operand
+    new_concat = int(str(current) + str(operand))
     
     depth += 1
-    if depth >= maxdepth:
-        return 0
+    if depth == maxdepth:
+        return new_plus == value or new_mult == value or new_concat == value
     
     return (solve_equation(value, subvalues, new_plus, depth, maxdepth) 
-            or solve_equation(value, subvalues, new_mult, depth, maxdepth))
+            or solve_equation(value, subvalues, new_mult, depth, maxdepth)
+            or solve_equation(value, subvalues, new_concat, depth, maxdepth))
 
 s = 0
 for equation in equations:
-    s += solve_equation(equation[0], equation[1], 0, 0, len(equation[1]))
+    s += equation[0] if solve_equation(equation[0], equation[1], 0, 0, len(equation[1])) else 0
 print(s)
