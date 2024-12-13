a9d6c5c0ab725796c84f398bd66d6c8d6bc06e78
Lienep <neilp@live.fr>
2024-12-13 17:18:34 +0100

    Finished part 1

---
+++ Day 13/part1.py
@@ -0,0 +1,56 @@
+import re
+
+equations = []
+
+with open("input.txt") as file:
+    while (line := file.readline().strip()):
+        coefficients = []
+
+        while line.startswith("Button"):
+            coefficients.append([int(x) for x in re.findall(r'\+(\d*)', line)])
+            line = file.readline().strip()
+
+        targets = [int(x) for x in re.findall(r'=(\d+)', line)]
+
+        equations.append([coefficients, targets])
+
+        file.readline()
+
+def PGCD(a, b):
+    r = a
+    while r != 0:
+        r = a % b
+        a = b
+        b = r
+    return a
+
+def PPCM(a, b):
+    return (a * b) // PGCD(a, b)
+
+def solve_system(coefficients, targets):
+    X, Y = targets
+
+    a, c = coefficients[0][0], coefficients[0][1]
+    b, d = coefficients[1][0], coefficients[1][1]
+
+    ppcm = PPCM(a,c)
+    coeff_l1 = ppcm // a
+    coeff_l2 = ppcm // c
+
+    y = (coeff_l2 * Y - coeff_l1 * X) / (d * coeff_l2 - b * coeff_l1)
+
+    if y != int(y):
+        return None
+
+    y = int(y)
+    x = (X - b * y) // a
+
+    return (x, y)
+
+s = 0
+for equation in equations:
+    result = solve_system(equation[0], equation[1])
+    if result != None:
+        s += result[0] * 3 + result[1] * 1
+
+print(s)
