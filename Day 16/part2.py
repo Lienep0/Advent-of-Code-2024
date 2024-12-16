4eab7ee1cf1d4419d6b88adc174718bc499d40a8
Lienep <neilp@live.fr>
2024-12-16 21:21:13 +0100

    Finished part 2

---
+++ Day 16/part2.py
@@ -0,0 +1,42 @@
+import heapq as hp
+
+DIRECTIONS = [(-1, 0), (0, 1), (1, 0), (0, -1)] # Up, Right, Down, Left
+
+lab = []
+with open("input.txt", "r") as file:
+    for line in file:
+        lab.append(list(line.strip()))
+
+startpos = (len(lab) - 2, 1)
+endpos = (1, len(lab[0]) - 2)
+
+bestscores = [[[float("inf")] * 4 for _ in line] for line in lab]
+bestendscore = float("inf")
+bestendpaths = set()
+
+heap = []
+hp.heappush(heap, (0, *startpos, 1, [startpos]))
+
+while len(heap) > 0:
+    score, i, j, og_dir, path = hp.heappop(heap)
+    if not score > bestendscore:
+        bestscores[i][j][og_dir] = score    
+        if (i, j) == endpos:
+            bestendscore = score
+            bestendpaths.update(path)
+
+        for k, dir in enumerate(DIRECTIONS):
+            if not (k == (og_dir + 2) % 4): # Can't turn backwards
+                dir_i, dir_j = dir
+                new_i, new_j = i + dir_i, j + dir_j
+
+                match lab[new_i][new_j]:
+                    case "#":
+                        pass
+                    case _:
+                        if k == og_dir and bestscores[new_i][new_j][k] > score + 1:
+                            hp.heappush(heap, (score + 1, new_i, new_j, k, path + [(new_i, new_j)]))
+                        elif bestscores[new_i][new_j][k] > score + 1000:
+                            hp.heappush(heap, (score + 1000, i, j, k, path))
+
+print(len(bestendpaths))
