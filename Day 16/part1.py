951ca40fba20926a4d04dedfb97d0a7ad2e20441
Lienep <neilp@live.fr>
2024-12-16 19:57:02 +0100

    Updated part 1 to actually use a heap, improving efficiency by 100x

--- Day 16/part1.py
+++ Day 16/part1.py
@@ -1,7 +1,9 @@
+import heapq as hp
+
 DIRECTIONS = [(-1, 0), (0, 1), (1, 0), (0, -1)] # Up, Right, Down, Left
 
 lab = []
-with open("easyinput.txt", "r") as file:
+with open("input.txt", "r") as file:
     for line in file:
         lab.append(list(line.strip()))
 
@@ -9,26 +11,31 @@ startpos = (len(lab) - 2, 1)
 endpos = (1, len(lab[0]) - 2)
 
 bestscores = [[[float("inf")] * 4 for _ in line] for line in lab]
+bestendscore = float("inf")
 
-heap = [(*startpos, 1, 0)]
+heap = []
+hp.heappush(heap, (0, *startpos, 1))
 
 while len(heap) > 0:
-    i, j, og_dir, score = heap.pop()
+    score, i, j, og_dir = hp.heappop(heap)
+    if not score > bestendscore:
+        bestscores[i][j][og_dir] = score    
+        if (i, j) == endpos:
+            bestendscore = score
 
-    bestscores[i][j][og_dir] = score
+        for k, dir in enumerate(DIRECTIONS):
+            if not (k == (og_dir + 2) % 4): # Can't turn backwards
+                dir_i, dir_j = dir
+                new_i, new_j = i + dir_i, j + dir_j
 
-    for k, dir in enumerate(DIRECTIONS):
-        dir_i, dir_j = dir
-        new_i, new_j = i + dir_i, j + dir_j
-
-        match lab[new_i][new_j]:
-            case "#":
-                pass
-            case _:
-                if k == og_dir and bestscores[new_i][new_j][k] > score + 1:
-                    heap.append((new_i, new_j, k, score + 1))
-                elif bestscores[new_i][new_j][k] > score + 1001:
-                    heap.append((new_i, new_j, k, score + 1001))
+                match lab[new_i][new_j]:
+                    case "#":
+                        pass
+                    case _:
+                        if k == og_dir and bestscores[new_i][new_j][k] > score + 1:
+                            hp.heappush(heap, (score + 1, new_i, new_j, k))
+                        elif bestscores[new_i][new_j][k] > score + 1001:
+                            hp.heappush(heap, (score + 1001, new_i, new_j, k))
 
 ei, ej = endpos
-print(bestscores[ei][ej])
+print(bestendscore)
