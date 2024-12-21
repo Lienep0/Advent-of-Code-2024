90fadae7da8adc8dd88cb014a28033d0cb8e6c1e
Lienep0 <neip@live.fr>
2024-12-21 16:03:36 +0100

    Finished part 1

---
+++ Day 20/part1.py
@@ -0,0 +1,50 @@
+import sys
+
+DIRECTIONS = [(-1, 0), (0, 1), (1, 0), (0, -1)] # Up, Right, Down, Left
+
+race, startfound, endfound, startpos, endpos = [], False, False, (-1, -1), (-1, -1)
+with open("input.txt", "r") as file:
+    for i, line in enumerate(file):
+        line = line.strip()
+        for j, char in enumerate(line):
+            if not startfound and char == "S":
+                startfound, startpos = True, (i, j)
+            if not endfound and char == "E":
+                endfound, endpos = True, (i, j)
+        race.append(line)
+
+height = len(race)
+width = len(race[0])
+sys.setrecursionlimit(height * width)
+
+grid_scores = [[0 for _ in line] for line in race]
+cheats = []
+
+def build_data(x, y, score, dir):
+    grid_scores[x][y] = score
+
+    if (x, y) != endpos:
+        for direction in DIRECTIONS:
+            dir_x, dir_y = direction
+            new_x, new_y = x + dir_x * 2, y + dir_y * 2
+
+            if not (new_x < 0 or new_x >= height or new_y < 0 or new_y >= width):
+                match race[new_x][new_y]:
+                    case "#":
+                        pass
+                    case _:
+                        cheats.append((score, new_x, new_y))
+
+        for k, direction in enumerate(DIRECTIONS):
+            if k != dir:
+                dir_x, dir_y = direction
+                new_x, new_y = x + dir_x, y + dir_y
+
+                match race[new_x][new_y]:
+                    case "#":
+                        pass
+                    case _:
+                        return build_data(new_x, new_y, score + 1, (k + 2) % 4)
+build_data(*startpos, 0, -1)
+
+print(sum([1 for score in [grid_scores[new_x][new_y] - score - 2 for score, new_x, new_y in cheats] if score >= 100]))
