fdb4434b1756c145ab179c831cfd462645563d61
Lienep0 <neip@live.fr>
2024-12-24 18:45:29 +0100

    Cleaner backtrack

--- Day 17/part2.py
+++ Day 17/part2.py
@@ -2,14 +2,16 @@ goal = list(reversed([2, 4, 1, 5, 7, 5, 1, 6, 0, 3, 4, 2, 5, 5, 3, 0]))
 
 def backtrack(A, i):
     if i == len(goal):
-        return A >> 3
+        return A
     
+    A <<= 3
     for k in range(8):
         A += k
         if (A ^ 5 ^ 6 ^ (A >> ((A % 8) ^ 5))) % 8 == goal[i]:
-            try: return backtrack(A << 3, i + 1)
+            try: return backtrack(A, i + 1)
             except BaseException: pass
         A -= k
+    A >>= 3
 
     raise BaseException
 
