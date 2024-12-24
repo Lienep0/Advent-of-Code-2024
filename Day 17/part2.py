c3cf3b0a002e9fdd6ef29ccd1985530204e8625c
Lienep0 <neip@live.fr>
2024-12-24 18:36:49 +0100

    FINALLY finished part 2

--- Day 17/part2.py
+++ Day 17/part2.py
@@ -1,35 +1,16 @@
-def bin3_to_int(string):
-    c = 0
-    bits = reversed([string[x:x+3] for x in range(0, len(string), 3)])
+goal = list(reversed([2, 4, 1, 5, 7, 5, 1, 6, 0, 3, 4, 2, 5, 5, 3, 0]))
 
-    for i, bit in enumerate(bits):
-        c += int(bit, 2) * (8 ** i)
-    return c
-
-goal = [1, 7, 2, 6, 4, 6, 2, 5, 3, 0, 7, 1, 6, 6, 0, 3] # (prog ^ 3 since 5^6 = 3)
-base_8 = [format(i, '03b') for i in range(8)]
-
-# We build A 3 bits at a time starting from the most significant since they reprensent the code's last digit
-A = (["0"] * 9) + ["0" for _ in range(len(goal) * 3)]
-
-# Rlly need to build A from the 3 leftmost digits and then bitshift 3 <<
 def backtrack(A, i):
-    if i < 0:
-        print("Found")
-        return A
+    if i == len(goal):
+        return A >> 3
     
-    bit_goal = base_8[goal[i]]
-    print(''.join(A))
-    for bit in base_8: # 3 leftmost bits of A (excluding shift buffer)
-        a = bin3_to_int(bit)
-        A_index = 9 + (len(goal) - i - 1) * 3
-        A_index_shift = a ^ 5
-        bit_output = base_8[(a ^ (bin3_to_int(''.join(A[A_index - A_index_shift - 3 : A_index])))) % 8]
-        if bit_output == bit_goal:
-            end = backtrack(A[:9 + A_index] + [x for x in bit_goal] + A[9 + A_index + 3:], i - 1)
-            if end != None:
-                return end
-            
-    return None
+    for k in range(8):
+        A += k
+        if (A ^ 5 ^ 6 ^ (A >> ((A % 8) ^ 5))) % 8 == goal[i]:
+            try: return backtrack(A << 3, i + 1)
+            except BaseException: pass
+        A -= k
 
-print(backtrack(A, len(goal) - 1))
+    raise BaseException
+
+print(backtrack(0, 0))
